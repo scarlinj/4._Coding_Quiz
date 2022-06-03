bc0649d5d0e0fa756d811f4728ec74842edf3287
Stephen Carlin <scarlinj@gmail.com>
2022-06-02 19:43:35 -0700

    added questions and highScores js. Not complete

---
+++ assets/js/questions.js
@@ -0,0 +1,68 @@
+var liveQuestions = [
+    {
+                question: "What are the three first languages of this course?",
+                choices: ["HTML, CSS, Javascript", "Hotmail, Fandango, Gmail", "Spanish, English, German"],
+                answer: "HTML, CSS, Javascript"
+                },
+            //     answer: 'a'
+            // },
+
+// below not working.  Test another method.
+    {
+        question: "What are the three first languages of this course?",
+        choices: {
+            a: 'HTML, CSS, Javascript',
+            b: 'Hotmail, Fandango, Gmail',
+            c: 'Spanish, English, German'
+        },
+        answer: 'a'
+    },
+    {
+        question: "How would you connect a css file to an html page?",
+        choices: {
+            a: 'Install the CSS in your browser',
+            b: 'Click the button to attach CSS to HTML',
+            c: 'Add the tag for the script in your HTML to include src='
+        },
+        answer: 'c'
+    },
+    {
+        question: "What element in your HTML allows you to crate a list?",
+        choices: {
+            a: '<li></li>',
+            b: '<ol></ol>',
+            c: 'Both a and b'
+        },
+        answer: 'c'
+    },
+    {
+        question: "How would you add an entire list item, listItemEl, to a list?",
+        choices: {
+            a: '.addChild(listItemEl',
+            b: '.appendChild(listItemEl);',
+            c: '.connectTheList(listItemEl);'
+        },
+        answer: 'b'
+    },
+    {
+        question: "If you create a function within curly braces {} for another function, what happens when you reference that function outside of the parent function?",
+        choices: {
+            a: 'The program will locate the function as normal.',
+            b: 'The program to break because it cannot find a variable with that name.',
+            c: 'The program will tell you to try again.'
+        },
+        answer: 'b'
+    },
+];
+
+var userScore= 0;
+var questionIndex = 0;
+
+var currentTime = document.querySelector("#currentTime");
+var timer = document.querySelector("#timer");
+var questionsDiv = document.querySelector("#questionsDiv");
+var wrapper = document.querySelector("#wrapper");
+
+timer.addEventListener("click", function () {
+    if ()
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
+var myTimer = function() {
+	if (time > 0) {
+		time = time - 1;
+		pageContentEl('#time').innerHTML = time;
+	} else {
+		clearInterval(clock);
+		pageContentEl('#score').innerHTML = score;
+		onlyDisplaySection("#finish");
+	}
+};
+
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
     function showQuestions(questions, quizContainer){
-        
+        var output = [];
+        var answers;
     }
 
     function showResults(questions, quizContainer, resultsContainer){
@@ -14,50 +26,197 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
     }
 };
 
-var liveQuestions = [
-    {
-        question: "What are the three first languages of this course?",
-        answers: {
-            a: 'HTML, CSS, Javascript',
-            b: 'Hotmail, Fandango, Gmail',
-            c: 'Spanish, English, German'
-        },
-        correctAnswer: 'a'
-    },
-    {
-        question: "How would you connect a css file to an html page?",
-        answers: {
-            a: 'Install the CSS in your browser',
-            b: 'Click the button to attach CSS to HTML',
-            c: 'Add the tag for the script in your HTML to include src='
-        },
-        correctAnswer: 'c'
-    },
-    {
-        question: "What element in your HTML allows you to crate a list?",
-        answers: {
-            a: '<li></li>',
-            b: '<ol></ol>',
-            c: 'Both a and b'
-        },
-        correctAnswer: 'c'
-    },
-    {
-        question: "How would you add an entire list item, listItemEl, to a list?",
-        answers: {
-            a: '.addChild(listItemEl',
-            b: '.appendChild(listItemEl);',
-            c: '.connectTheList(listItemEl);'
-        },
-        correctAnswer: 'b'
-    },
-    {
-        question: "If you create a function within curly braces {} for another function, what happens when you reference that function outside of the parent function?",
-        answers: {
-            a: 'The program will locate the function as normal.',
-            b: 'The program to break because it cannot find a variable with that name.',
-            c: 'The program will tell you to try again.'
-        },
-        correctAnswer: 'b'
-    },
-];
+var initialTime = 50;
+var time = 50;
+var score = 0;
+var quizCount = 0;
+var setTime;
+var answers = document.querySelectorAll('#quizHolder button');
+var clock;
+var recordsArray = [];
+
+
+// var randomQuestion = questions[Math.floor(math.random() * questions.length)];
+
+var pageContentEl = function(element) {
+	return document.querySelector(element);
+};
+
+var myTimer = function() {
+	if (time > 0) {
+		time = time - 1;
+		pageContentEl('#time').innerHTML = time;
+	} else {
+		clearInterval(clock);
+		pageContentEl('#score').innerHTML = score;
+		onlyDisplaySection("#finish");
+	}
+};
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+
+var onlyDisplaySection = function(element) {
+	let sections = document.querySelectorAll("section");
+	Array.from(sections).forEach(function(userItem) {
+		userItem.classList.add('hide');
+	});
+	pageContentEl(element).classList.remove('hide');
+};
+
+var quizUpdate = function(answerCopy) {
+	pageContentEl('#scoreAlert p').innerHTML = answerCopy;
+	pageContentEl('#scoreAlert').classList.remove('invisible', scoreAlert());
+	Array.from(answers).forEach(answer =>
+	{
+		answer.classList.add('disable');
+	});
+
+	setTimeout(function() {
+		if (quizCount === questions.length) {
+			onlyDisplaySection("#finish");
+			time = 0;
+			pageContentEl('#time').innerHTML = time;
+		} else {
+			setQuestionData();
+			Array.from(answers).forEach(answer => {
+				answer.classList.remove('disable');
+			});
+		}
+	}, 1000);
+};
+
+//nth-of-type is working better than assigning an individual button id: https://www.w3schools.com/cssref/sel_nth-of-type.asp
+
+var setQuestionData = function() {
+	pageContentEl('#quizHolder p').innerHTML = questions[quizCount].title;
+	pageContentEl('#quizHolder button:nth-of-type(1)').innerHTML = `1. ${questions[quizCount].choices[0]}`;
+	pageContentEl('#quizHolder button:nth-of-type(2)').innerHTML = `2. ${questions[quizCount].choices[1]}`;
+	pageContentEl('#quizHolder button:nth-of-type(3)').innerHTML = `3. ${questions[quizCount].choices[2]}`;
+	pageContentEl('#quizHolder button:nth-of-type(4)').innerHTML = `4. ${questions[quizCount].choices[3]}`;
+};
+
+var scoreAlert = function() {
+	clearTimeout(setTime);
+	setTime = setTimeout(function() {
+		pageContentEl('#scoreAlert').classList.add('invisible');
+	}, 1000);
+};
+
+var errorAlert = function() {
+	clearTimeout(setTime);
+	setTime = setTimeout(function() {
+		pageContentEl('#errorAlert').classList.add('invisible');
+	}, 3000);
+};
+
+var enterInitials = function() {
+	let initialsRecord = pageContentEl('#initials').value;
+	if (initialsRecord === ''){
+		pageContentEl('#errorAlert p').innerHTML = "You need at least 1 character";
+		pageContentEl('#errorAlert').classList.remove('invisible', errorAlert());
+	} else if (initialsRecord.match(/[[A-Za-z]/) === null) {
+		pageContentEl('#errorAlert p').innerHTML = "Only letters for initials allowed.";
+		pageContentEl('#errorAlert').classList.remove('invisible', errorAlert());
+	} else if (initialsRecord.length > 5) {
+		pageContentEl('#errorAlert p').innerHTML = "Maximum of 5 characters allowed.";
+		pageContentEl('#errorAlert').classList.remove('invisible', errorAlert());
+	} else {
+		recordsArray.push({
+			"initialRecord": initialsRecord,
+			"score": score
+		});
+		
+		localStorage.setItem('recordsArray', JSON.stringify(recordsArray));
+		pageContentEl('#highScores div').innerHTML = '';
+		onlyDisplaySection("#highScores");
+		recordsHtmlReset();
+		pageContentEl("#initials").value = '';
+		}
+};
+
+var clearHighScores = function () {
+	recordsArray = [];
+	pageContentEl('#highScores div').innerHTML = "";
+	localStorage.removeItem('recordsArray');
+};
+
+var quizReset = function () {
+	time = initialTime;
+	score = 0;
+	quizCount = 0;
+	onlyDisplaySection("#intro");
+};
+
+var startQuiz = function () {
+    setQuestionData();
+	onlyDisplaySection("#quizHolder");
+	clock = setInterval(myTimer, 1000);
+};
+
+// https://teamtreehouse.com/community/dont-understand-the-meaning-of-functione
+
+// https://stackoverflow.com/questions/35936365/what-exactly-is-the-parameter-e-event-and-why-pass-it-to-javascript-functions
+
+var viewHighScores = function (e) {
+	e.preventDefault();
+	clearInterval(clock);
+	pageContentEl('#time').innerHTML = 0;
+	time = initialTime;
+	score = 0;
+	quizCount = 0;
+	onlyDisplaySection("#highScores");
+	recordsHtmlReset();
+};
+
+var scoreTimeAdjust = function () {
+	if (this.innerHTML.substring(3, this.length) === questions[quizCount].answer) {
+		score = score + 1;
+		quizCount = quizCount + 1;
+		quizUpdate("Correct");
+	}else{
+		time = time - 5;
+		quizCount = quizCount + 1;
+		quizUpdate("Incorrect");
+	}
+};
+
+// https://www.w3schools.com/js/js_arrow_function.asp
+
+var recordsHtmlReset = function() {
+	pageContentEl('#highScores div').innerHTML = "";
+	let i = 1;
+	recordsArray.sort((a, b) => b.score - a.score);
+	Array.from(recordsArray).forEach(check =>
+	{
+		var scores = document.createElement("div");
+		scores.innerHTML = i + ". " + check.initialRecord + " - " + check.score;
+		pageContentEl('#highScores div').appendChild(scores);
+		i = i + 1;
+	});
+	i = 0;
+	Array.from(answers).forEach(answer => {
+		answer.classList.remove('disable');
+	});
+};
+
+// found where I can use ? in lieu of an if statement: https://medium.com/javascript-in-plain-english/what-does-the-question-mark-mean-in-javascript-code-353cfadcf760
+
+// https://www.geeksforgeeks.org/ternary-operator-question-mark-and-colon-in-javascript/
+
+// condition ? value if true : value if false
+
+(localStorage.getItem('recordsArray')) ? recordsArray = JSON.parse(localStorage.getItem('recordsArray')): recordsArray = [];
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
+
+Array.from(answers).forEach(check => {check.addEventListener('click', scoreTimeAdjust);});
+
+pageContentEl("#intro button").addEventListener("click", startQuiz);
+
+pageContentEl("#records button").addEventListener("click", enterInitials);
+
+pageContentEl("#clearScores").addEventListener("click", clearHighScores);
+
+pageContentEl("#reset").addEventListener("click", quizReset);
+
+pageContentEl("#scores").addEventListener("click", viewHighScores);
